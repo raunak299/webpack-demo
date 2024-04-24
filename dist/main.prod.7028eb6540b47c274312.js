771f92648d23fe572c64c1ccb4f5efcc1b8c55dd
raunak299 <iamraunak299@gmail.com>
2024-04-24 16:10:58 +0530

    build cleaned

--- dist/main.prod.7028eb6540b47c274312.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkwebpack_demo=self.webpackChunkwebpack_demo||[]).push([[792],{44:(e,n,c)=>{c.e(894).then(c.t.bind(c,543,23)).then((({default:{join:e}})=>{const n=document.createElement("div");return n.innerHTML=e(["Hello","webpack"]," "),n})).catch((e=>"An error occurred while loading the component")).then((e=>{document.body.appendChild(e)}))}},e=>{e(e.s=44)}]);
-//# sourceMappingURL=main.prod.7028eb6540b47c274312.js.map
