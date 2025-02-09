381d27ae9d4ed1e6960177a738f285d3e11f763b
raunak299 <iamraunak299@gmail.com>
2025-02-10 00:57:29 +0530

    add more detail

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const { merge } = require("webpack-merge");
 const modeConfig = (env) => require(`./build-utils/webpack.${env.mode}`)(env);
-const presetConfig = require("./build-utils/loadPresets");
 
 module.exports = (env) => {
-  const { mode,presets=[] } = env;
+  const { mode, presets = [] } = env;
 
   const baseConfig = {
     mode: mode,
@@ -43,17 +42,13 @@ module.exports = (env) => {
       new MiniCssExtractPlugin({
         filename: "styles.css",
       }),
-      // //removing it for now , not working somehow
-      //  new webpack.DllReferencePlugin({
-      //       manifest: path.join(__dirname, './dist', 'vendor-manifest.json')
-      //     }),
     ],
     // used to split the code into separate chunks and improve the performance of the application and achieve better caching
     optimization: {
       moduleIds: "deterministic",
-      // Setting runtimeChunk: 'single' in Webpack optimizes the build by ensuring that only one runtime chunk is generated for multiple entry point also separate runtime code from main bundle, leading to better caching and potentially smaller bundle sizes. If you don't specify 'single', Webpack may generate multiple runtime chunks, which could impact caching efficiency and result in slightly larger bundles.
+      // Webpack may generate multiple runtime chunks, which could impact caching efficiency and result in slightly larger bundles.
       runtimeChunk: "single",
-      // splitChunks focuses on optimizing the code split into separate chunk
+      //Extracts third-party dependencies (node_modules/) into a separate chunk named node_modules_vendor.js, reducing the main bundle size.
       splitChunks: {
         cacheGroups: {
           vendor: {
@@ -64,18 +59,7 @@ module.exports = (env) => {
         },
       },
     },
-    cache: {
-      // Enable caching
-      //By default, webpack's built-in cache is stored in memory and is not persistent across different builds.
-      //In some scenarios, such as CI/CD environments or multi-stage build pipelines, it may be advantageous to use a persistent cache stored on disk.
-      // This can help maintain cache consistency and improve build performance, especially in environments where webpack instances are frequently started and stopped.
-      type: "filesystem",
-      cacheDirectory: path.resolve(__dirname, ".webpack_cache"),
-      buildDependencies: {
-        config: [__filename],
-      },
-    },
   };
 
-  return merge(baseConfig, modeConfig(env), presetConfig({ mode, presets }));
+  return merge(baseConfig, modeConfig(env));
 };
